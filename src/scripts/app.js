577d4a8da754deed1e0a1f68f117d135c75e3ce1
Oliver Inglis <ollie@theinglistwins.com>
2020-07-03 20:06:28 +0100

    animations added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import style from '../styles/index.scss';
+import { fadeCssAnimation } from './fade_animations.js';
 import '../fonts/FavoritPro-Regular.otf';
 
 
 
+fadeCssAnimation();
